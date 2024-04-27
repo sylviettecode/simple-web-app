2fefcf3046e10f09630a04c88c685cbc2aaf561d
sylviette <no@email.com>
2024-04-27 03:12:05 +0300

    styling and old testing ip

--- frontend/script.js
+++ frontend/script.js
@@ -1,26 +1,27 @@
-fetch('http://10.0.0.16:5000/products')           .then(response => response.json())
-            .then(data => {
-                const tableBody = document.getElementById('table_body');
-                data.forEach(product => {
-                    const row = document.createElement('tr');
+fetch('localhost:5000/products')          
+    .then(response => response.json())
+.then(data => {
+    const tableBody = document.getElementById('table_body');
+    data.forEach(product => {
+        const row = document.createElement('tr');
 
-                    const idCell = document.createElement('td');
-                    idCell.textContent = product.product_id;
-                    row.appendChild(idCell);
+        const idCell = document.createElement('td');
+        idCell.textContent = product.product_id;
+        row.appendChild(idCell);
 
-                    const nameCell = document.createElement('td');
-                    nameCell.textContent = product.product_name;
-                    row.appendChild(nameCell);
+        const nameCell = document.createElement('td');
+        nameCell.textContent = product.product_name;
+        row.appendChild(nameCell);
 
-                    const priceCell = document.createElement('td');
-                    priceCell.textContent = product.product_price;
-                    row.appendChild(priceCell);
+        const priceCell = document.createElement('td');
+        priceCell.textContent = product.product_price;
+        row.appendChild(priceCell);
 
-                    const countCell = document.createElement('td');
-                    countCell.textContent = product.product_count;
-                    row.appendChild(countCell);
+        const countCell = document.createElement('td');
+        countCell.textContent = product.product_count;
+        row.appendChild(countCell);
 
-                    tableBody.appendChild(row);
-                });
-            })
-            .catch(error => console.error('Error fetching data:', error));
+        tableBody.appendChild(row);
+    });
+})
+.catch(error => console.error('Error fetching data:', error));
